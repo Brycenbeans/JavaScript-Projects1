e0c4cbc7478b75052d8e3e2b4ed9214dd011c472
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2021-11-24 15:52:16 -0800

    Add files via upload

---
+++ JavaScript-Projects/Project1_expressions_alert/JS/alert.js
@@ -0,0 +1,19 @@
+var C ="\"This is a quotation\"" //window alert and quotation//
+window.alert(C);
+
+document.write ("\"This is a printed quote "
++ "that I typed\""); //this prints the thing//
+var B = "concatenated" + "string" //this is concatenate//
+var B = B.fontcolor("blue");
+document.write(B)
+
+var Family = "The Blacks", Dad = "Barndog", Mom = "Mom"; //changing string variable font color//
+var Dad = Dad.fontcolor("blue");
+document.write(Dad)
+document.write(Mom)
+
+var blues = "I have the blues";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3*3); //printing the result of 3*3 operation//
---
+++ JavaScript-Projects/README.md/README.md
@@ -0,0 +1,2 @@
+# JavaScript-Projects
+Collection of all projects completed on The Tech Academy Bootcamp
